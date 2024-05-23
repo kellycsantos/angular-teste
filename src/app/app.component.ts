0ddcd627f52394966dcd725797ac1436adbea58d
Kelly Ramos <kramos@fcc.org.br>
2024-05-23 17:15:32 -0300

    primeiro projeto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BotaoComponent } from './botao/botao.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
